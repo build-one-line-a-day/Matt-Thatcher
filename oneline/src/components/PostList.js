1c5717ade47a2c02e0dcca3163d24df8a4bdd1b4
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-29 14:51:40 -0600

    added react-router, added semantic-ui, added PostList component

---
+++ oneline/src/components/PostList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PostList() {
+    return (
+        <div>
+            <h1>Entries List</h1>
+            <h3>Date:</h3>
+            <h3>Title:</h3>
+        </div>
+    )
+}
