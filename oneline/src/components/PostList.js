eb0868bf2b19be5b8ed2fc1158ef9df17c2fbbbd
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-29 15:27:34 -0600

    added a template from semantic-ui

--- oneline/src/components/PostList.js
+++ oneline/src/components/PostList.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { List, Segment } from 'semantic-ui-react'
 
 export default function PostList() {
     return (
-        <div>
-            <h1>Entries List</h1>
-            <h3>Date:</h3>
-            <h3>Title:</h3>
-        </div>
+        <Segment inverted>
+            <List divided inverted relaxed>
+                <List.Item>
+                    <List.Content>
+                        <List.Header>Snickerdoodle</List.Header>
+                        An excellent companion
+                    </List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Content>
+                        <List.Header>Poodle</List.Header>
+                        A poodle, its pretty basic
+                    </List.Content>
+                </List.Item>
+                <List.Item>
+                    <List.Content>
+                        <List.Header>Paulo</List.Header>
+                        He's also a dog
+                    </List.Content>
+                </List.Item>
+            </List>
+        </Segment>
     )
 }
